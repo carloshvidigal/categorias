6669211ffc4cec17009b02cba3b789e1be329937
carloshvidigal <chmovi@hotmail.com>
2025-12-11 12:23:49 -0300

    Criar o projeto categorias da webAPI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,53 @@
-import { Routes } from '@angular/router';
+//import { AuthGuardService } from './../service/auth-guard.service';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+import { Categorias } from './categorias/categorias';
+import { CategoriaDetalhe } from './categoria-detalhe/categoria-detalhe';
+import { CategoriaNova } from './categoria-nova/categoria-nova';
+import { CategoriaEditar } from './categoria-editar/categoria-editar';
+import { Login } from './login/login';
+import { Logout } from './logout/logout';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login,
+    data: { title: 'Login' }
+  },
+  {
+    path: 'logout',
+    component: Logout,
+    data: { title: 'Logout' }
+  },
+  {
+    path: 'categorias',
+    component: Categorias,
+    data: { title: 'Lista de Categorias' }
+  },
+  {
+    path: 'categoria-detalhe/:id',
+    component: CategoriaDetalhe,
+    data: { title: 'Detalhe da Categoria' }
+  },
+  {
+    path: 'categoria-nova',
+    component: CategoriaNova,
+    data: { title: 'Adicionar Categoria' }
+  },
+  {
+    path: 'categoria-editar/:id',
+    component: CategoriaEditar,
+    data: { title: 'Editar a Categoria' }
+  },
+  { path: '',
+    redirectTo: '/categorias',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
